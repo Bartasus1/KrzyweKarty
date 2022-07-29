b88d562a48795ee7160750c20eca31f004d55111
Bartłomiej Banaś <62613502+Bartasus1@users.noreply.github.com>
2022-07-29 20:35:16 +0200

    Update RoundManager.cpp

--- Source/KrzyweKarty/Gameplay/RoundManager.cpp
+++ Source/KrzyweKarty/Gameplay/RoundManager.cpp
@@ -12,7 +12,6 @@ URoundManager::URoundManager()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = false;
-	SetIsReplicated(true);
 
 	CharactersUsedInRound.Reserve(3);
 	// ...
