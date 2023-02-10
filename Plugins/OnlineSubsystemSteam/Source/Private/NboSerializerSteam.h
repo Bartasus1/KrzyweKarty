1ef40d9d6f8eaa183419753312a7aa7b9f3eefff
Bartłomiej Banaś <62613502+Bartasus1@users.noreply.github.com>
2023-02-10 16:48:22 +0100

    Steam Online Subsystem update (Steam connections working finally)

---
+++ Plugins/OnlineSubsystemSteam/Source/Private/NboSerializerSteam.h
@@ -0,0 +1,70 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OnlineSubsystemSteamTypes.h"
+#include "NboSerializer.h"
+
+/**
+ * Serializes data in network byte order form into a buffer
+ */
+class FNboSerializeToBufferSteam : public FNboSerializeToBuffer
+{
+public:
+	/** Default constructor zeros num bytes*/
+	FNboSerializeToBufferSteam() :
+		FNboSerializeToBuffer(512)
+	{
+	}
+
+	/** Constructor specifying the size to use */
+	FNboSerializeToBufferSteam(uint32 Size) :
+		FNboSerializeToBuffer(Size)
+	{
+	}
+
+	/**
+	 * Adds Steam session info to the buffer
+	 */
+ 	friend inline FNboSerializeToBufferSteam& operator<<(FNboSerializeToBufferSteam& Ar, const FOnlineSessionInfoSteam& SessionInfo)
+ 	{
+		check(SessionInfo.HostAddr.IsValid());
+		// Skip SessionType (assigned at creation)
+		Ar << *SessionInfo.HostAddr;
+		Ar << SessionInfo.SessionId->UniqueNetId;
+		return Ar;
+ 	}
+};
+
+/**
+ * Class used to write data into packets for sending via system link
+ */
+class FNboSerializeFromBufferSteam : public FNboSerializeFromBuffer
+{
+public:
+	/**
+	 * Initializes the buffer, size, and zeros the read offset
+	 */
+	FNboSerializeFromBufferSteam(uint8* Packet,int32 Length) :
+		FNboSerializeFromBuffer(Packet,Length)
+	{
+	}
+
+	/**
+	 * Reads Steam session info from the buffer
+	 */
+ 	friend inline FNboSerializeFromBufferSteam& operator>>(FNboSerializeFromBufferSteam& Ar, FOnlineSessionInfoSteam& SessionInfo)
+ 	{
+		check(SessionInfo.HostAddr.IsValid());
+
+		// Skip SessionType (assigned at creation)
+		Ar >> *SessionInfo.HostAddr;
+
+		uint64 SessionId;
+		Ar >> SessionId;
+		SessionInfo.SessionId = FUniqueNetIdSteam::Create(SessionId);
+
+		return Ar;
+ 	}
+};
